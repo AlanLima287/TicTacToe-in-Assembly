6a9ccf0d460d099994aef75a58b4bed210675d05
AlanLima287 <alanlimanvc@gmail.com>
2024-09-07 16:36:42 -0300

    Human readable messages

--- src/tictactoe.s
+++ src/tictactoe.s
@@ -20,20 +20,15 @@ start:
    int 0x10
 
    mov si, board
-print_board:
-   lodsb
-   cmp al, 0
-   je main
-   
-   mov ah, 0x0E
-   int 0x10
-   jmp print_board
-main:
-   mov bp, 0x0058
+   call print
+
    xor ax, ax
    mov [ptr_cursor], ax
    mov [ptr_O_marks], ax
    mov [ptr_X_marks], ax
+   
+   mov al, 0x4F
+   mov [ptr_player], al
    jmp put_cursor
 get_arrow_key:
    mov ah, 0
@@ -80,8 +75,11 @@ put_mark:
    test bx, [ptr_X_marks]
    jnz get_arrow_key
 
-   xor bp, 0x0017
-   cmp bp, 0x004F
+   mov al, [ptr_player]
+   xor al, 0x17
+   mov [ptr_player], al
+
+   cmp al, 0x4F
    jne handle_X_mark
 
 handle_O_mark:
@@ -108,7 +106,7 @@ fix_overflow_col:
    jmp put_cursor
 
 print_mark:
-   mov ax, bp
+   mov al, [ptr_player]
    mov ah, 0x0E
    int 0x10
 
@@ -116,74 +114,52 @@ print_mark:
    not ax
 
    xor bx, bx
-tests:
+.tests:
    test ax, [win_boards + bx]
-   jz exit
+   jz win_exit
    
    add bx, 2
    cmp bx, 0x10
-   jne tests
+   jne .tests
 
-   mov ax, 0x00B4
    mov bx, [ptr_O_marks]
    or bx, [ptr_X_marks]
-
    cmp bx, 0x01FF
-   cmove bp, ax
-   je exit
+   je tie_exit
 
 put_cursor:
    mov [ptr_cursor], dx
-   
-   xor bx, bx
+   mov bx, 0x0000
    mov ax, 0x0200
-   mov dx, 0x0300
-   int 0x10
-
-   mov bx, [ptr_O_marks]
-   mov cx, 0x0209
-   
-loop:
-   sub cl, 1
-
-   mov dx, bx
-   shr dx, cl
-   and dx, 1
-   mov al, 0x30
-   add al, dl
-   mov ah, 0x0E
-   int 0x10
-   
-   cmp cl, 0
-   jne loop
-   
-   mov ax, 0x0E20
-   int 0x10
-
-   mov cl, 0x09
-   sub ch, 1
-   mov bx, [ptr_X_marks]
-   
-   test ch, 1
-   jnz loop
-
-   mov bh, 0x00
-   mov ah, 0x02
-   mov dx, [ptr_cursor]
    int 0x10
 
    jmp get_arrow_key
 
-exit:
+win_exit:
    mov bh, 0x00
    mov ah, 0x02
    mov dx, 0x0400
    int 0x10
 
-   mov ax, bp
+   mov al, [ptr_player]
    mov ah, 0x0E
    int 0x10
+   
+   mov si, win_message
+   call print
+   jmp exit
 
+tie_exit:
+   mov bh, 0x00
+   mov ah, 0x02
+   mov dx, 0x0400
+   int 0x10
+
+   mov si, tie_message
+   call print
+
+   ;jmp exit
+exit:
    mov ah, 0
    int 0x16
 
@@ -198,10 +174,31 @@ exit:
    cli
    hlt
 
+print:
+   push bp
+   mov bp, sp
+
+.loop:
+   lodsb
+   cmp al, 0
+   je .func
+   
+   mov ah, 0x0E
+   int 0x10
+   jmp .loop
+
+.func:
+   mov sp, bp
+   pop bp
+   ret
+
 ptr_O_marks dw 0x0000
 ptr_X_marks dw 0x0000
 ptr_cursor dw 0x0000
+ptr_player db 0x4F
 board db " | | ", 0xA, 0xD, " | | ", 0xA, 0xD, " | | ", 0
 win_boards dw 0x0007, 0x0038, 0x01C0, 0x0049, 0x0092, 0x0124, 0x0111, 0x0054
+win_message db " won!!!", 0
+tie_message db "It was a tie!", 0
 times 510 - ($ - $$) db 0
 dw 0xAA55
